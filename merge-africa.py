c95f55cc9f9de028867cd894dec4467cc3a5c880
Victoria Neema <victoria@kartoza.com>
2024-07-04 23:49:37 +0300

    Update merge-africa script to work for Kenya

--- merge-africa.py
+++ merge-africa.py
@@ -1,62 +1,60 @@
 #!/usr/bin/env python3
 
-import shapely
 import fiona
-
+from shapely.geometry import mapping, shape
 from shapely.ops import unary_union
-from shapely.geometry import shape, mapping
 
 # Don't even bother extracting the data, we can read the shapefile
-source_dataset = '/vsizip/data/ne_10m_admin_0_countries.zip'
-source_exclusions = 'data/exclusions.json'
+source_dataset = "/vsizip/data/ne_10m_admin_0_countries.zip"
+source_exclusions = "data/exclusions.json"
 
-dest_dataset = 'africa-extent.json'
-dest_bbox = 'africa-extent-bbox.json'
+dest_dataset = "kenya-extent.json"
+dest_bbox = "kenya-extent-bbox.json"
 
-# Filter for Africa
-def africa_region_un(rec):
-    return rec['properties'].get('REGION_UN') == 'Africa'
 
-def filter_further(rec):
-    # Filtering out Saint Helena based on Adam Lewis' email 13 May 2020
-    return rec['properties'].get('NAME') != 'Saint Helena'
+# Filter for Kenya
+def filter_by_name(rec):
+    return rec["properties"].get("NAME") == "Kenya"
+
 
 # Open the data, filter it and turn it into a list of Shapely features
 world = fiona.open(source_dataset)
-africa = filter(africa_region_un, world)
-africa_filtered = filter(filter_further, africa)
-africa_shapely = [shape(c['geometry']) for c in africa_filtered]
+kenya = filter(filter_by_name, world)
+kenya_shapely = [shape(c["geometry"]) for c in kenya]
 
 # Union the file (merge all the features together)
-africa_unioned = unary_union(africa_shapely)
+kenya_unioned = unary_union(kenya_shapely)
 # Create a convex hull (minimum bounding shape)
-africa_convex_hull = africa_unioned.convex_hull
+kenya_convex_hull = kenya_unioned.convex_hull
 # Buffer it by 2 degrees (means we get an extra scene around the coastlines)
-africa_hull_buffer = africa_convex_hull.buffer(1.0)
+kenya_hull_buffer = kenya_convex_hull.buffer(1.0)
 
 # Exclude some areas
 exclusions = fiona.open(source_exclusions)
-exclusions_shapely = unary_union([shape(c['geometry']) for c in exclusions])
+exclusions_shapely = unary_union([shape(c["geometry"]) for c in exclusions])
 
-africa_hull_buffer_exclusions = africa_hull_buffer.difference(exclusions_shapely)
+kenya_hull_buffer_exclusions = kenya_hull_buffer.difference(exclusions_shapely)
 
 # Define a schema to write to
 schema = {
-    'geometry': 'Polygon',
-    'properties': {'bbox': 'str'},
+    "geometry": "Polygon",
+    "properties": {"bbox": "str"},
 }
 
 # And write the output to geojson
-with fiona.open(dest_dataset, 'w', 'GeoJSON', schema) as out:
-    out.write({
-        'geometry': mapping(africa_hull_buffer_exclusions),
-        'properties': {'bbox': str(africa_hull_buffer_exclusions.envelope.bounds)}
+with fiona.open(dest_dataset, "w", "GeoJSON", schema) as out:
+    out.write(
+        {
+            "geometry": mapping(kenya_hull_buffer_exclusions),
+            "properties": {"bbox": str(kenya_hull_buffer_exclusions.envelope.bounds)},
         }
     )
 
 # Also write the bbox, because why not?!
-with fiona.open(dest_bbox, 'w', 'GeoJSON', schema) as out:
-    out.write({
-        'geometry': mapping(africa_hull_buffer_exclusions.envelope),
-        'properties': {'bbox': str(africa_hull_buffer_exclusions.envelope.bounds)}
-    })
+with fiona.open(dest_bbox, "w", "GeoJSON", schema) as out:
+    out.write(
+        {
+            "geometry": mapping(kenya_hull_buffer_exclusions.envelope),
+            "properties": {"bbox": str(kenya_hull_buffer_exclusions.envelope.bounds)},
+        }
+    )
