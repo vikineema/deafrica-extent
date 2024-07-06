3e677ac610ae3299e2d2665339d2e5845556d188
Victoria Neema <victoria@kartoza.com>
2024-07-06 16:31:46 +0300

    Format using black

--- merge-africa.py
+++ merge-africa.py
@@ -5,7 +5,7 @@ from shapely.geometry import mapping, shape
 from shapely.ops import unary_union
 
 # Don't even bother extracting the data, we can read the shapefile
-source_dataset = '/vsizip/data/ne_10m_admin_0_countries.zip'
+source_dataset = "/vsizip/data/ne_10m_admin_0_countries.zip"
 source_exclusions = "data/exclusions.json"
 
 dest_dataset = "kenya-extent.json"
